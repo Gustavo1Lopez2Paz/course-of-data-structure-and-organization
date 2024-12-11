f1365ed03f925315987086a61671801b2ef7ba2d
Gustavo Lopez Paz <230110531@itsoeh.edu.mx>
2024-12-10 23:56:57 -0600

    Stacks: v1 hecho
    Signed-off-by: Gustavo Lopez Paz <230110531@itsoeh.edu.mx>

--- learning/Stacks/stack_sandbox.py
+++ learning/Stacks/stack_sandbox.py
@@ -2,38 +2,44 @@ from Node import Node
 import logging
 
 class Stack:
-    def __init__(self, limit = 1000, size = 0):
+    def __init__(self):
         self.top_item = None
-        self.limit = limit
-        self.size = size
-        
-    def push(self, value):
-        if self.has_space():
-            item = Node(value)
-            item.set_next_node(self.top_item)
-            self.top_item = item
-            self.size += 1
-        else:
-            logging.warning("La pila esta llena ¡No queda espacio!")
-
-    def pop(self):
-        if not self.is_empty():
-            item_to_remove = self.top_item
-            self.top_item = self.top_item.get_next_node()
-            self.size -= 1
-            return item_to_remove.get_value()
-        else:
-            logging.warning("La pila esta totalmente vacia!")
-
     def peek(self):
-        if not self.is_empty():
-            return self.top_item.get_value()
-        else:
-            logging.warning("La pila esta totalmente vacia!")
+     return self.top_item.get_value()
 
-    def has_space(self):
-        return self.limit > self.size
+# class Stack:
+#     def __init__(self, limit = 1000, size = 0):
+#         self.top_item = None
+#         self.limit = limit
+#         self.size = size
+        
+#     def push(self, value):
+#         if self.has_space():
+#             item = Node(value)
+#             item.set_next_node(self.top_item)
+#             self.top_item = item
+#             self.size += 1
+#         else:
+#             logging.warning("La pila esta llena ¡No queda espacio!")
+
+#     def pop(self):
+#         if not self.is_empty():
+#             item_to_remove = self.top_item
+#             self.top_item = self.top_item.get_next_node()
+#             self.size -= 1
+#             return item_to_remove.get_value()
+#         else:
+#             logging.warning("La pila esta totalmente vacia!")
+
+#     def peek(self):
+#         if not self.is_empty():
+#             return self.top_item.get_value()
+#         else:
+#             logging.warning("La pila esta totalmente vacia!")
+
+#     def has_space(self):
+#         return self.limit > self.size
     
-    def is_empty(self):
-        return self.size == 0
+#     def is_empty(self):
+#         return self.size == 0
     
